343ae2f08b368e1a379ad0b17c408371e42efd0d
Gonzalez Dario <darioggonzalez98@gmail.com>
2024-03-29 03:15:26 -0300

    fix some styles

---
+++ challenge-dev/src/components/RickAndMortyLogo.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/RickandMorty.svg'
+
+const RickAndMortyLogo = () => {
+    return (
+        <div>
+            <img src={logo} style={{ maxWidth: '500px' }}/>
+        </div>
+    )
+}
+
+export default RickAndMortyLogo
